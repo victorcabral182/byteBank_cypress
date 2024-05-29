0e8bea9516b35726a5a12817d65c63d0d0c613f0
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2024-05-28 21:14:49 -0300

    feat: starting new course of the formation

--- front/cypress/e2e/formularioLogin.cy.js
+++ front/cypress/e2e/formularioLogin.cy.js
@@ -1,22 +1,20 @@
-describe('Formulário de Login', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+describe('Formulario de Login', ()=>{
+  beforeEach(()=>{
+    cy.visit('http://localhost:3000')
   })
-  it('Não deve permitir um email inválido', () => {
+
+  it('Não deve permitir um email inválido', ()=>{
     cy.getByData('botao-login').click()
     cy.getByData('email-input').type('neilton@alura')
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro')
-      .should('exist')
-      .and('have.text', 'O email digitado é inválido')
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
   })
-  it('Não deve permitir um campo em branco', () => {
+
+  it.only('Não deve permitir um campo em branco', ()=>{
     cy.getByData('botao-login').click()
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro')
-      .should('exist')
-      .and('have.text', 'O campo email é obrigatório')
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
   })
 })
